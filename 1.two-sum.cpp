b237763a6c01356a0c391ca509c068a12ef01b85
Sonu Bhardwaj <sbhardwaj1315@gmail.com>
2026-06-18 23:12:11 +0530

    Initial commit

---
+++ 1.two-sum.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            for(int j = i + 1; j < nums.size(); j++) {
+
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
+// @lc code=end
+// @lc code=end
+
